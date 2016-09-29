981b6234e2c76de9379c9b9a9c2896cf1df083e3
SajidYarKhan <sajid.yarkhan@yahoo.com>
2016-09-29 15:56:40 -0500

    Sajid WebService Exercise

---
+++ TuscService/Controllers/TransactionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuscData;
+
+namespace TuscService.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        // GET api/transactions
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> Get()
+        {
+            return DataManager.GetTransactions();
+        }
+
+        // GET api/transactions
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> GetTransactionsByDate(DateTime dt1, DateTime dt2)
+        {
+            return DataManager.GetTransactions().Where(r => r.Date >= dt1 && r.Date <= dt2);
+        }
+    }
+}
