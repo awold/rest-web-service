99040dfa155ad0de05b405c2e31f7209705a99d2
afoat <aarron.foat@gmail.com>
2016-09-29 14:49:05 -0500

    Implemented UsersController and TransactionsController

---
+++ TuscService/Controllers/TransactionsController.cs
@@ -0,0 +1,27 @@
+﻿namespace TuscService.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
+    using TuscData;
+
+    public class TransactionsController : ApiController
+    {
+        // GET api/products
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> Get()
+        {
+            return DataManager.GetTransactions();
+        }
+        
+        // GET api/users?sortBalance=ASC
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> GetTransactionsByDate(DateTime startDate, DateTime endDate)
+        {
+            return DataManager.GetTransactions().Where(t => (t.Date.Date >= startDate.Date && t.Date.Date <= endDate.Date));
+        }
+    }
+}
