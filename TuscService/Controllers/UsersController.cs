d5cfa226b7f2f92a8ba9e20cda771a8215ba2d9c
gagan5133 <ght7348@varian.com>
2016-09-29 16:17:41 -0500

    COmpleted.

---
+++ TuscService/Controllers/UsersController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+using TuscData;
+
+namespace TuscService.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        [Route("users")]
+        public IEnumerable<User> Get()
+        {
+            return DataManager.GetUsers();
+        }
+
+        [HttpGet]
+        [Route("users/{id}")]
+        public User Get(int id)
+        {
+            return DataManager.GetUsers().Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        [Route("users")]
+        public int? Post([FromBody]User user)
+        {
+            return DataManager.CreateUser(user);
+        }
+
+        [HttpPut]
+        [Route("users/{id}")]
+        public void Put(int id, [FromBody]User user)
+        {
+            user.Id = id;
+
+            DataManager.UpdateUser(user);
+        }
+
+
+        [HttpDelete]
+        [Route("users/{id}")]
+        public void Delete(int id)
+        {
+            DataManager.DeleteUser(id);
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public IEnumerable<User> GetUser(string sortBalance)
+        {
+            if (sortBalance == "ASC")
+            {
+                return DataManager.GetUsers().OrderBy(v => v.Balance).ToList();
+            }
+            else if (sortBalance == "DESC")
+            {
+                return DataManager.GetUsers().OrderByDescending(v => v.Balance).ToList();
+            }
+            return null;
+        }
+    }
+}
