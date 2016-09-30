501446f4f3972f7ebf715405a7b6b7f5c3a12097
VarianErik <Erik.Peterson@varian.com>
2016-09-30 11:32:28 -0500

    Done.

--- TuscService/Controllers/ProductsController.cs
+++ TuscService/Controllers/ProductsController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Routing;
-
 using TuscData;
 
 namespace TuscService.Controllers
@@ -25,8 +24,7 @@ namespace TuscService.Controllers
         [Route("products")]
         public IEnumerable<Product> GetProductsWithStock(bool hasStock)
         {
-            // TODO
-            return null;
+            return hasStock ? DataManager.GetProducts().Where(p => p.Quantity > 0) : DataManager.GetProducts().Where(p => p.Quantity == 0);
         }
 
         // GET api/products/5
---
+++ TuscService/Controllers/TransactionsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+using TuscData;
+
+namespace TuscService.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        // GET api/<controller>
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> Get()
+        {
+            return DataManager.GetTransactions();
+        }
+
+        // GET /transactions?startDate=2016-01-01&endDate=2016-12-31
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> GetTransactionsInDateRange(DateTime startDate, DateTime endDate)
+        {
+            return DataManager.GetTransactions().Where(d=>d.Date > startDate).Where(d=>d.Date < endDate);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
