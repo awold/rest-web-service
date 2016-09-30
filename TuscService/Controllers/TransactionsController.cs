c2d97f2a16ed9cc3fdce44b7dde7144b0d3f56b2
Chris Jeffers <Chris.Jeffers@varian.com>
2016-09-30 10:50:23 -0500

    Done

---
+++ TuscService/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuscData;
+namespace TuscService.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        // GET api/products
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> Get()
+        {
+            return DataManager.GetTransactions();
+        }
+
+
+        // GET api/<controller>/5
+        [HttpGet]
+        [Route("transactions")]
+        public IEnumerable<Transaction> Get(DateTime startDate, DateTime endDate)
+        {
+            return DataManager.GetTransactions().Where(p => p.Date >= startDate && p.Date <= endDate);
+        }
+    }
+}
---
+++ TuscService/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuscData;
+
+namespace TuscService.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // GET api/products
+        [HttpGet]
+        [Route("users")]
+        public IEnumerable<User> Get()
+        {
+            return DataManager.GetUsers();
+        }
+
+        // GET api/products/5
+        [HttpGet]
+        [Route("users/{id}")]
+        public User Get(int id)
+        {
+            return DataManager.GetUsers().Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public IEnumerable<User> Get(string sortBalance)
+        {
+            if (sortBalance.Equals("ASC"))
+            {
+                return DataManager.GetUsers().OrderBy(p => p.Balance);
+            }
+            else
+            {
+                return DataManager.GetUsers().OrderByDescending(p => p.Balance);
+            }
+        }
+
+        [HttpPost]
+        [Route("users")]
+        public int? Post([FromBody] User user)
+        {
+            return DataManager.CreateUser(user);
+        }
+
+        // PUT api/products/5
+        [HttpPut]
+        [Route("users/{id}")]
+        public void Put(int id, [FromBody]User user)
+        {
+            user.Id = id;
+
+            DataManager.UpdateUser(user);
+        }
+
+        [HttpDelete]
+        [Route("users/{id}")]
+        public void Delete(int id)
+        {
+            DataManager.DeleteUser(id);
+        }
+    }
+}
